41d4d3f0e2b8e925811f2226365c94c5d9075d14
Andrei Muntean <andrei.muntean.dev@gmail.com>
2016-02-10 21:53:26 +0000

    Added Emacs and Sublime Text configurations

---
+++ Emacs/init.el
@@ -0,0 +1,16 @@
+;; Disables the toolbar.
+(tool-bar-mode -1)
+
+;; Sets the default theme.
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'purple-haze t)
+
+;; Configures Sicstus Prolog.
+(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
+(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
+(autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
+(setq prolog-system 'sicstus)
+(setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
+				("\\.m$" . mercury-mode))
+			      auto-mode-alist))
+(setq prolog-indent-width 4)
